16bea5adddd07fa7cb8b2cce4847d03af6dfbd51
Daniel <d0583258844@gmail.com>
2026-02-03 10:15:10 +0200

    change the matrix to simpla arr each position called element

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let elements = [];
 
 function createMatrix() {
-  const n = 30;
-  const m = 50;
+  const n = 1500;
 
   for (let i = 0; i <= n; i++) {
-    elements.push(new Array(m).fill(" "));
+    elements.push(" ");
   }
   return elements;
 }
