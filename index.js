0a1184f702ea3a622b74047a5e9e2c6eb165e7d6
Daniel <d0583258844@gmail.com>
2026-02-03 16:27:37 +0200

    change few things inside the js file add maindiv thet connatin all the tils. and list tiles with all the tiles names to run on each one and change to sky

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
+const board = document.getElementById("board");
 
-function createMatrix() {
-  let elements = [];
+function createBord() {
   const n = 3003;
   for (let i = 0; i < n; i++) {
     const div = document.createElement("div");
+    div.classList.add("tile");
     if (i < 1000) {
-      div.className = "allElements sky";
-      div.id = `place${i}`
-      elements.push(div);
-    } 
-    else if (1000 < i && i < 1101) {
-      div.className = "allElements grass";
-      div.id = `place${i}`
-      elements.push(div);
+      div.classList.add("sky");
+      div.id = `place${i}`;
+    } else if (999 < i && i < 1100) {
+      div.classList.add("grass");
+      div.id = `place${i}`;
+    } else if (1099 < i && i < 1500) {
+      div.classList.add("graound");
+      div.id = `place${i}`;
+    } else if (1499 < i && i < 2800) {
+      div.classList.add("gray");
+      div.id = `place${i}]`;
+    } else if (2799 < i && i < 3003) {
+      div.classList.add("rocks");
+      div.id = `place${i}`;
     }
-    else if (1101 < i && i < 1502) {
-      div.className = "allElements graound";
-      div.id = `place${i}`
-      elements.push(div);
-    }
-    else if (1501 < i && i < 2802) {
-      div.className = "allElements gray";
-      div.id = `place${i}`
-      elements.push(div);
-    } else if (2802 < i && i < 3003) {
-      div.className = "allElements rocks";
-      div.id = `place${i}`
-      elements.push(div);
-    }
-    document.body.append(...elements);
+    board.append(div);
   }
 }
 
-createMatrix();
+createBord();
+board.addEventListener("click", (e) => {
+  if (e.target.classList.contains("tile")) {
+    const listClassList = Array.from(e.target.classList)
+    e.target.classList.remove(listClassList.find(c=>c!=="tile"));
+    e.target.classList.add("grass");
+  }
+});
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createtree() {
+  const numbers = randomInt(1, 100);
+}
